e60d2690b6b35468176b2b569d3693f973f804f2
lwch <lwch748@gmail.com>
2022-03-02 01:22:15 +0800

    添加框架代码

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import "os"
+
+func usage() {
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "create":
+	case "destroy":
+	case "start":
+	case "stop":
+	case "attach":
+	}
 }
